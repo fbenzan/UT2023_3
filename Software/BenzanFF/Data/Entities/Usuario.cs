4decc2a0603e78d17ad9da77532b8a24ac5ca181
Felix Benzan <felixbenz07@gmail.com>
2023-08-04 01:39:48 -0400

    StringExtention
    Create StringExtention
    Use StringExtention

--- Software/BenzanFF/Data/Entities/Usuario.cs
+++ Software/BenzanFF/Data/Entities/Usuario.cs
@@ -1,10 +1,39 @@
-﻿namespace BenzanFF.Data.Entities;
+﻿using BenzanFF.Extentions;
+
+namespace BenzanFF.Data.Entities;
 
 public class Usuario : Entity
 {
+    public Usuario()
+    {
+        
+    }
+    public Usuario(string name, string nickname, string password)
+    {
+        Name = name;
+        Nickname = nickname;
+        Password = password.Encriptar();
+        Role = "basic";
+    }
+    public Usuario(string name, string nickname, string password, string role)
+    {
+        Name = name;
+        Nickname = nickname;
+        Password =  password.Encriptar();
+        Role = role;
+    }
+
     public string Name { get; set; } = null!;
     public string Nickname { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string Role { get; set; } = null!;
+    public static Usuario Crear(string name, string nickname, string password) =>
+        new(name,nickname,password);
+    public static Usuario Crear(string name, string nickname, string password,string role) =>
+        new(name, nickname, password,role);
+    public bool Authorized(string password)
+    {
+        return Password == password.Encriptar();
+    }
 }
 
---
+++ Software/BenzanFF/Extentions/StringExtention.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BenzanFF.Extentions
+{
+    public static class StringExtention
+    {
+        public static string Encriptar(this string texto)
+        {
+            using SHA256 sha256Hash = SHA256.Create();
+            byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+
+            // Utilizamos un formato más conciso para convertir el array de bytes en una cadena hexadecimal
+            return BitConverter.ToString(data).Replace("-", "").ToLower();
+        }
+    }
+}
