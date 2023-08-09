9f963cca5890b0d8e0098e802a5adbf18259f424
Felix Benzan <felixbenz07@gmail.com>
2023-08-09 09:03:06 -0400

    Se agregar el contexto y se inicializan los datos luego de la migracion.

---
+++ Software/BenzanFF/Data/Contexts/BFFMyDbContextSeeder.cs
@@ -0,0 +1,30 @@
+﻿using BenzanFF.Data.Entities;
+
+namespace BenzanFF.Data.Contexts;
+//Esta clase es para inicializar datos en la base de datos.
+public class BFFMyDbContextSeeder
+{
+    public static async Task Inicializar(BFFMyDbContext db)
+    {
+        await GenerarCategorias(db);
+    }
+
+    private static async Task GenerarCategorias(BFFMyDbContext db)
+    {
+        //En caso de no existir categorías, agregar las categorías por defecto.
+        if (!db.Categotias.Any())
+        {
+            //Variables para las categorias nuevas.
+            var categorias_nuevas = new List<ServicioCategoria>()
+            {
+                new (){ Nombre = "Guarniciones"},
+                new (){ Nombre = "Carnes"},
+                new (){ Nombre = "Hamburguesas"},
+            };
+            //Se agregar a la tabla del contexto de la base de datos.
+            db.Categotias.AddRange(categorias_nuevas);
+            //Se guardan los cambios en la base de datos.
+            await db.SaveChangesAsync();
+        }
+    }
+}
--- Software/BenzanFF/Program.cs
+++ Software/BenzanFF/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BenzanFF.Data;
+using BenzanFF.Data.Contexts.Interfaces;
+using BenzanFF.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+//Se agregar el conexto de la base de datos como contexto. 
+builder.Services.AddDbContext<BFFMyDbContext>();
+//Se confgura la inyeccion de dependencias del contexto.
+builder.Services.AddScoped<IBFFMyDbContext, BFFMyDbContext>();
 
 var app = builder.Build();
 
@@ -27,5 +34,14 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+//Actualizar la base de datos segun las migraciones...
+using (var scope = app.Services.GetService<IServiceScopeFactory>()!.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<BFFMyDbContext>();
+    //Se actualiza la base de datos si hay migraciones pendientes
+    dbContext.Database.Migrate();
+    //Se inicializan los datos, si es requerido...
+    await BFFMyDbContextSeeder.Inicializar(dbContext);
+}
 
-app.Run();
+    app.Run();
