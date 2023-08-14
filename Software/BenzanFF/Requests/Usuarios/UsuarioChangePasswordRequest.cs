7583456cd5e264bd646f4734b83618791c752761
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2023-08-13 21:52:50 -0400

    Mejoras al cambiar la contrasena del usuario desde el perfil

--- Software/BenzanFF/Requests/Usuarios/UsuarioChangePasswordRequest.cs
+++ Software/BenzanFF/Requests/Usuarios/UsuarioChangePasswordRequest.cs
@@ -1,10 +1,15 @@
-﻿namespace BenzanFF.Requests.Usuarios;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BenzanFF.Requests.Usuarios;
 
 public class UsuarioChangePasswordRequest
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "La contraseña actual del usuario es obligatoria."), DataType(DataType.Password)]
     public string OldPassword { get; set; } = "";
+    [Required(ErrorMessage = "La contraseña del usuario es obligatoria."), DataType(DataType.Password), StringLength(8, MinimumLength = 4, ErrorMessage = "La nueva contraseña debe tener entre 4 y 8 caracteres.")]
     public string NewPassword { get; set; } = "";
+    [Required(ErrorMessage = "La contraseña de confirmación del usuario es obligatoria."), DataType(DataType.Password), Compare("NewPassword", ErrorMessage = "Las contraseñas no coinciden")]
     public string NewPasswordConfirm { get; set; } = "";
     public bool Change => (NewPassword == NewPasswordConfirm && !string.IsNullOrWhiteSpace(OldPassword));
     public void Reset()
